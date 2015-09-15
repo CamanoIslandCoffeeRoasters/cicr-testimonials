c003e362bf9f6eac3fd849eb07ce3b9cd0d1d609
tobinfekkes <tobin@camanoislandcoffee.com>
2015-09-15 17:27:16 -0400

    [+] Added customer submitable reviews, and performed loads of stylistic fixes and improvements.

--- ajax/testimonials.php
+++ ajax/testimonials.php
@@ -7,51 +7,70 @@ require($_SERVER['DOCUMENT_ROOT'] . '/wp-load.php');
 //					  Buckle up, Buttercup.		jk			//
 //////////////////////////////////////////////////////////////
 
-global $wpdb;
-$args = array(	'posts_per_page' => 3,
-				'offset' => 0,
-				'order' => 'ASC',
-				'orderby' => 'rand',
-				'post_type' => 'testimonial',
-				'post_status' => 'publish');
-$review_counter = 1;
-$reviews = get_posts($args);
-foreach ($reviews as $review) {
-	if($review_counter == 2){ $margin = '';} else{$margin = 'margin-top: 10%;';}
-	$id = "bubbleNumber" . $review_counter;
+if (isset($_POST['testimonial'])) {
+	global $current_user;
+	echo $current_user->user_firstname;
+	var_dump($_POST);
 	
-	$i=0;
-	$stars= "";
-	while($i < $review->stars) {
-		$stars = $stars . '<div class="star-five "></div>';
-		$i++;
+	$my_post = array(
+	  'post_title'    => wp_strip_all_tags($current_user->user_firstname),
+	  'post_content'  => $_POST['testimonial'],
+	  'post_status'   => 'pending',
+	  'post_author'   => 1,
+	  'post_type' 	  => 'testimonial'
+	);
+	
+	// Insert the post into the database
+	$postId = wp_insert_post( $my_post );
+	add_post_meta($postId, 'stars', $_POST['rating']);
+	
+} else {
+	global $wpdb;
+	$args = array(	'posts_per_page' => 3,
+					'offset' => 0,
+					'order' => 'ASC',
+					'orderby' => 'rand',
+					'post_type' => 'testimonial',
+					'post_status' => 'publish');
+	$review_counter = 1;
+	$reviews = get_posts($args);
+	foreach ($reviews as $review) {
+		if($review_counter == 2){ $margin = '';} else{$margin = 'margin-top: 10%;';}
+		$id = "bubbleNumber" . $review_counter;
+		
+		$i=0;
+		$stars= "";
+		while($i < $review->stars) {
+			$stars = $stars . '<div class="star-five "></div>';
+			$i++;
+		}
+		$class = '';
+		$background = '';
+		$background = substr_replace(wp_get_attachment_url( get_post_thumbnail_id($review->ID)), '', 7, 0);
+		
+		if (strlen($background) > 5) {
+			$background = "background: url(" . $background . ");";
+		}
+		
+		echo "
+				<div class='bubble' style=' $margin;\" id='" . $id	 . "'>
+							<div class='bubble-image $class' style='" . $background . "'></div>
+						<div class='first-row'>
+								<h3>$review->post_title	</h3>
+						</div>
+								<div class='star-row'>
+									$stars
+								</div>
+						<div class='review-content bubble-text'>
+							<p class='line-clamp '>$review->post_content</p>
+						</div>
+							<div class='more-button textBubbleNumber-$review_counter' style='display: none;'>  MORE  </div>
+							<div class='more-button hidden-textBubbleNumber-$review_counter' style='display: none;'>  LESS  </div>
+				</div>
+		";
+		$review_counter ++;
 	}
-	$class = '';
-	$background = '';
-	$background = substr_replace(wp_get_attachment_url( get_post_thumbnail_id($review->ID)), '', 7, 0);
-	
-	if (strlen($background) > 5) {
-		$background = "background: url(" . $background . ");";
-	}
-	
-	echo "
-			<div class='bubble' style=' $margin;\" id='" . $id	 . "'>
-						<div class='bubble-image $class' style='" . $background . "'></div>
-					<div class='first-row'>
-							<h3>$review->post_title	</h3>
-					</div>
-							<div class='star-row'>
-								$stars
-							</div>
-					<div class='review-content bubble-text'>
-						<p class='line-clamp '>$review->post_content</p>
-					</div>
-						<div class='more-button textBubbleNumber-$review_counter' style='display: none;'>  MORE  </div>
-						<div class='more-button hidden-textBubbleNumber-$review_counter' style='display: none;'>  LESS  </div>
-			</div>
-	";
-	$review_counter ++;
-}
-	
+		
 return $reviews;
+}
 ?>
--- testimonials-options.php
+++ testimonials-options.php
@@ -1,3 +1,4 @@
+
 <?php
 /*
 Plugin Name: CICR Testimonials Plugin
@@ -76,12 +77,29 @@ function review_callback($atts, $content = '')
     $a       = shortcode_atts(array(
         'id' => '0',
         'color' => 'white',
-        'time' => '11'
+        'time' => '11',
+        'backend' => 0,
+        'stars' => 0
     ), $atts);
     $opacity = 1;
     $color   = isset($a['color']) ? $a['color'] : "blue";
     $id      = isset($a['id']) ? $a['id'] : "0";
     $time    = isset($a['time']) ? $a['time'] : "9";
+	if ($a['backend'] == 1) {
+		$backend = "<div class='review-button'><h3 id='reviewAreaActuator'>What do YOU think?</h3></div>";
+	}
+	if ($a['stars'] == 0) {
+		$starDisplay = "display: none;";
+		$alignSubmit = "margin-left: 210px;";
+	} else {
+		$starDisplay = "
+    display: -webkit-box;
+    display: -moz-box;
+    display: -ms-flexbox;
+    display: -webkit-flex;
+    display: flex;";
+		$alignSubmit = "margin-left: 150px;";
+	}
     switch ($color) {
         case "blue":
             $background_color = "background: rgba(133, 170, 177, $opacity);";
@@ -111,7 +129,6 @@ function review_callback($atts, $content = '')
         case "beige":
             $background_color = "background:rgba(234, 214, 190, $opacity);";
             $color            = "color: #503D20;";
-            $loading_bar      = "rgba(234, 214, 190, $opacity);";
             break;
         
         case "grey":
@@ -142,55 +159,87 @@ function review_callback($atts, $content = '')
 	    	
 	    reviewSlider();
 	    
+	    $('#reviewAreaActuator').live('click', function() {
+	    	reviewarea();
+	    })
+	    
+	    function reviewarea() {
+	    	$('.slider-div').slideUp();
+	    	$('.review-area').slideDown();
+            window.clearInterval(timedInterval)
+            timedInterval = null;
+			progress($('.progressBar'), 'stop');
+	    	$('.review-module').animate({ height: 500} , 250);
+	    	document.getElementById('reviewAreaActuator').id = 'thankYou';
+	    	
+	    }
+	    
+	    $('.testimonialForm').submit(function(){
+	    	event.preventDefault();
+    		$.when($.ajax({type: 'POST', url: '/wp-content/plugins/cicr-testimonials/ajax/testimonials.php', data: $(this).serialize() })).done(function(result) {
+		    	console.log(result);
+		    	$('.review-area').slideUp();
+		    	$('.slider-div').slideDown();
+		    	$('.review-module').animate({ height: 700} , 250);
+		    	document.getElementById('thankYou').innerHTML = 'Thanks for the input!';
+	    		timedInterval = window.setInterval(animateSlider, " . $time . "000);
+				flipflop = 1;
+				progress($('.progressBar'), flipflop);
+				
+	    	})
+	    })
+	    
+	    
 	    var timedInterval = window.setInterval(animateSlider, " . $time . "000);
 	    
 	    $('.textBubbleNumber-1').live('click', function() {
-	        showhide(1);
-	    });
+	        showhide(1);});
 
 	    $('.textBubbleNumber-2').live('click', function() {
-	        showhide(2);
-	    });
+	        showhide(2);});
 
 	    $('.textBubbleNumber-3').live('click', function() {
-	        showhide(3);
-	    });
+	        showhide(3);});
 
 
 	    $('.hidden-textBubbleNumber-1').live('click', function() {
-	        showhide(1);
-	    });
+	        showhide(1);});
 
 	    $('.hidden-textBubbleNumber-2').live('click', function() {
-	        showhide(2);
-	    });
+	        showhide(2);});
 
 	    $('.hidden-textBubbleNumber-3').live('click', function() {
-	        showhide(3);
-	    });
+	        showhide(3);});
 		
 		
 		function progress(\$element, flipflop) {
 			if((flipflop % 2) == 1) {
-	    		\$element.find('div').animate({ width: '0' }, 1);
+	    		\$element.find('div').animate({ width: '0%' }, 1);
 	    		\$element.find('div').animate({ width: '100%' }, " . $time . "000); }
 			if((flipflop % 2) == 0) {
 	    		\$element.find('div').animate({ width: '28%' }, 1);
 	    		\$element.find('div').animate({ width: '0%' }, " . $time . "000); }
+			if(flipflop == 'stop') {
+				\$element.find('div').stop(true, false);
+	    		\$element.find('div').animate({ width: '0%' }, 1000);
+			}
 		}
 		
 	    function showhide(id) {
 	        if ($('.textBubbleNumber-' + id).is(':visible')) {
-	            $('.textBubbleNumber-' + id).hide();
-	            $('.hidden-textBubbleNumber-' + id).show('slow');
-	            $('.hidden-textBubbleNumber-' + id).show('slow');
 	            window.clearInterval(timedInterval)
 	            timedInterval = null;
+				progress($('.progressBar'), 'stop');
+	            $('.textBubbleNumber-' + id).hide();
+	            $('.hidden-textBubbleNumber-' + id).slideDown('fast');
+	            $('.hidden-textBubbleNumber-' + id).slideDown('fast');
 	        } else {
 	            $('.textBubbleNumber-' + id).show();
 	            $('.hidden-textBubbleNumber-' + id).hide();
 	            $('.hidden-textBubbleNumber-' + id).hide();
 	    		timedInterval = window.setInterval(animateSlider, " . $time . "000);
+				flipflop = 1;
+				progress($('.progressBar'), flipflop);
 	        }
 	    }
 		
@@ -202,7 +251,9 @@ function review_callback($atts, $content = '')
 		var flipflop = 1;
 			
 	    function reviewSlider() {
+	    	$('.review-area').hide();
 		$.when($.ajax({url: '/wp-content/plugins/cicr-testimonials/ajax/testimonials.php'})).done(function(result) {
+			if ((flipflop) == 1) {flipflop++};
             $('#review-bubble-area').html(result).fadeIn('slow');
 			progress($('.progressBar'), flipflop);
 				
@@ -211,7 +262,6 @@ function review_callback($atts, $content = '')
 				var text = shorten(element.textContent, 190, element);
                 element.textContent = text[1];
 				if (text[0] == 1) {
-					console.log(this);
 					this.className = 'textBubbleNumber-' + (index + 1)
 	            	$(this).hover(function() {
 	            		$(this).css('cursor', 'pointer')});
@@ -247,8 +297,17 @@ function review_callback($atts, $content = '')
 	<style>
 	@import url(http://fonts.googleapis.com/css?family=Open+Sans:400,600,700);
 	
+	
 .hidden-text {
 	cursor: pointer;
+}
+
+#thankYou {
+	cursor: default;
+}
+
+#reviewAreaActuator {
+    cursor: pointer;
 }
 
 .more-button {
@@ -277,12 +336,43 @@ function review_callback($atts, $content = '')
     color: #503D20;
 }
 
+.review-button {
+	width: 25%;
+	align-self: middle;
+    margin: 2% auto 2% auto;
+    border-style: solid;
+    border-width: 3px;
+    border-$color
+    background: transparent;
+    padding: 1% 2%;
+    -webkit-border-radius: 10px;
+    -moz-border-radius: 10px;
+    border-radius: 10px;
+    -webkit-box-shadow: rgba(0,0,0,1) 0 0px 0;
+    -moz-box-shadow: rgba(0,0,0,1) 0 0px 0;
+    box-shadow: rgba(0,0,0,1) 0 0px 0;
+    text-shadow: rgba(0,0,0,.4) 0 0px 0;
+    font-size: 100%;
+    font-weight: bold;
+    font-family: Ubuntu, Helvetica, Arial, Sans-Serif;
+    $color
+    align-self: flex-end;
+}
+.review-button h1,
+.review-button h2,
+.review-button h3,
+.review-button h4,
+.review-button h5,
+.review-button p {
+	$color;
+}
+
 .progressBar {
-    display: -webkit-box;      /* OLD - iOS 6-, Safari 3.1-6 */
-    display: -moz-box;         /* OLD - Firefox 19- (buggy but mostly works) */
-    display: -ms-flexbox;      /* TWEENER - IE 10 */
-    display: -webkit-flex;     /* NEW - Chrome */
-    display: flex;  
+    display: -webkit-box;
+    display: -moz-box;>
+    display: -ms-flexbox;
+    display: -webkit-flex;
+    display: flex;
 	justify-content: center;
 	-webkit-justify-content: center;
     min-width: 100%;
@@ -295,25 +385,25 @@ function review_callback($atts, $content = '')
 .progressBar div {
     height: 100%;
     color: #fff;
-    width: 0;
+    width: 100%;
     max-width: 100%;
     $background_color
 }
 
 .bubble h3 {
-		font-family: 'Open Sans', Arial, sans-serif;
-		color: #42321C;
-		margin-top: 30px;
-		margin-bottom: 10px;
+	font-family: 'Open Sans', Arial, sans-serif;
+	color: #42321C;
+	margin-top: 30px;
+	margin-bottom: 10px;
 }
 
 .bubble {
 	padding-top: 10px;
-    display: -webkit-box;      /* OLD - iOS 6-, Safari 3.1-6 */
-    display: -moz-box;         /* OLD - Firefox 19- (buggy but mostly works) */
-    display: -ms-flexbox;      /* TWEENER - IE 10 */
-    display: -webkit-flex;     /* NEW - Chrome */
-	align-self: center;
+    display: -webkit-box;
+    display: -moz-box;
+    display: -ms-flexbox;
+    display: -webkit-flex;
+    display: flex;
 	-webkit-align-self: center;
 	justify-content: space-around;
 	width: 300px;
@@ -326,14 +416,15 @@ function review_callback($atts, $content = '')
    align-items: center;
    -webkit-justify-content: center;
    justify-content: center;
+   margin-bottom: 20px;
 }
 
 .bubble-image {
-    display: -webkit-box;      /* OLD - iOS 6-, Safari 3.1-6 */
-    display: -moz-box;         /* OLD - Firefox 19- (buggy but mostly works) */
-    display: -ms-flexbox;      /* TWEENER - IE 10 */
-    display: -webkit-flex;     /* NEW - Chrome */
-    display: flex;  
+    display: -webkit-box;
+    display: -moz-box;
+    display: -ms-flexbox;
+    display: -webkit-flex;
+    display: flex;
 	width: 105px;
 	height: 105px;
 	border-radius: 75px;
@@ -350,11 +441,11 @@ function review_callback($atts, $content = '')
 
 .callout {
 	position: relative;
-    display: -webkit-box;      /* OLD - iOS 6-, Safari 3.1-6 */
-    display: -moz-box;         /* OLD - Firefox 19- (buggy but mostly works) */
-    display: -ms-flexbox;      /* TWEENER - IE 10 */
-    display: -webkit-flex;     /* NEW - Chrome */
-    display: flex;  
+    display: -webkit-box;
+    display: -moz-box;
+    display: -ms-flexbox;
+    display: -webkit-flex;
+    display: flex;
 	vertical-align: bottom;
 	right: -30%;
 	width: 0;
@@ -374,78 +465,95 @@ function review_callback($atts, $content = '')
 
 
 .star-row {
-  display: -webkit-box;      /* OLD - iOS 6-, Safari 3.1-6 */
-  display: -moz-box;         /* OLD - Firefox 19- (buggy but mostly works) */
-  display: -ms-flexbox;      /* TWEENER - IE 10 */
-  display: -webkit-flex;     /* NEW - Chrome */
-  display: flex;  
+    display: -webkit-box;
+    display: -moz-box;
+    display: -ms-flexbox;
+    display: -webkit-flex;
+    display: flex;
 	-webkit-align-content: middle;
-align-content: middle;
+	align-content: middle;
 	margin: 25px auto
 }
 
 .review-content {
-		margin: 0 10px 20px 10px;
-		width: 270px;
-		text-align: justify;
+	margin: 0 10px 20px 10px;
+	width: 270px;
+	text-align: justify;
 }
 
 .star-five {
+	$starDisplay
 	flex-grow: 1;
-		align-self: flex-start;
-		margin-top: 30px;
-		margin: 0 5px;
-		width: 25px;
-		height: 25px;
-		background: #42321C;
-		-webkit-clip-path: polygon(50% 0%, 63% 38%, 100% 38%, 69% 59%, 82% 100%, 50% 75%, 18% 100%, 31% 59%, 0% 38%, 37% 38%);
-		clip-path: polygon(50% 0%, 63% 38%, 100% 38%, 69% 59%, 82% 100%, 50% 75%, 18% 100%, 31% 59%, 0% 38%, 37% 38%);
+	align-self: flex-start;
+	margin-top: 30px;
+	margin: 0 5px;
+	width: 25px;
+	height: 25px;
+	background: #42321C;
+	-webkit-clip-path: polygon(50% 0%, 63% 38%, 100% 38%, 69% 59%, 82% 100%, 50% 75%, 18% 100%, 31% 59%, 0% 38%, 37% 38%);
+	clip-path: polygon(50% 0%, 63% 38%, 100% 38%, 69% 59%, 82% 100%, 50% 75%, 18% 100%, 31% 59%, 0% 38%, 37% 38%);
 }
 
  .review-module {
-      " . $background_color . "
-      margin-left: -15%;
-      margin-right:-15%;
-      -webkit-background-size: cover;
-      -moz-background-size: cover;
-      -o-background-size: cover;
-      background-size: cover;
-      min-height: 750px;
-      padding-top: 1;
-      padding-bottom: 10%;
-      position: static;
-      padding-left: 15%;
-      padding-right: 15%;
-      max-width:100%;
-      ' ." . $color . " . ';
-      text-align: center;
-  display: -webkit-box;      /* OLD - iOS 6-, Safari 3.1-6 */
-  display: -moz-box;         /* OLD - Firefox 19- (buggy but mostly works) */
-  display: -ms-flexbox;      /* TWEENER - IE 10 */
-  display: -webkit-flex;     /* NEW - Chrome */
-  display: flex;  
-      justify-content: space-between; 
-      flex-wrap: wrap;
+    " . $background_color . "
+    margin-left: -15%;
+    margin-right:-15%;
+    -webkit-background-size: cover;
+    -moz-background-size: cover;
+    -o-background-size: cover;
+    background-size: cover;
+    min-height: 700px;
+    padding-top: 1;
+    padding-bottom: 10%;
+    position: static;
+    padding-left: 15%;
+    padding-right: 15%;
+    max-width:100%;
+    ' ." . $color . " . ';
+    text-align: center;
+    display: -webkit-box;
+    display: -moz-box;
+    display: -ms-flexbox;
+    display: -webkit-flex;
+    display: flex;
+    justify-content: space-between; 
+    flex-wrap: wrap;
+    -webkit-flex:1 1 100%;
+    -webkit-flex-flow: row wrap;
  }
 
  #review-bubble-area {
- 	  display: -webkit-box;      /* OLD - iOS 6-, Safari 3.1-6 */
-  display: -moz-box;         /* OLD - Firefox 19- (buggy but mostly works) */
-  display: -ms-flexbox;      /* TWEENER - IE 10 */
-  display: -webkit-flex;     /* NEW - Chrome */
-  display: flex;  
-      justify-content: space-between; 
-      flex-wrap: wrap;
-      width:100%;
+    display: -webkit-box;
+    display: -moz-box;
+    display: -ms-flexbox;
+    display: -webkit-flex;
+    display: flex;
+    -webkit-justify-content: space-between;
+    justify-content: space-between; 
+    flex-wrap: wrap;
+    -webkit-flex:1 1 auto;
+	-webkit-flex-flow: row wrap;
+    width: 100%;
+	min-height: 450px;
  }
 
- .review-module h1{
+.review-area {
+    display: -webkit-box;
+    display: -moz-box;
+    display: -ms-flexbox;
+    display: -webkit-flex;
+    display: flex;
+	flex-direction: column;
+    -webkit-flex-flow: row wrap;
+    justify-content: space-around;
+	width: 100%;
+}
+
+.review-module h1{
     " . $color . "
     font-size: 5.0em;
     line-height: 1.1em;
     font-weight: 700;
-    
-
  }
 
  .review-module h2{
@@ -454,45 +562,131 @@ align-content: middle;
     line-height: 1.1em;
     font-weight: 700;
 	padding-top: 25px;
-
-    
-
  }
  
+ .review-area input {
+ 	margin: 5% -10%;
+	width: 500px;
+	height: 250px;
+ }
+ 
+  .review-area input::-webkit-input-placeholder { 	text-align:center;}
+  .review-area input:-moz-placeholder { text-align:center; }
  
  .review-module h4{
     " . $color . "
-
  }
  
  .review-module h5{
     " . $color . "
-
  }
  
  .review-module h6{
-    " . $color . "
-    font-size: 1.2em;
-    line-height: 1.1em;
-    text-align: left;
-
+	" . $color . "
+	font-size: 1.2em;
+	line-height: 1.1em;
+	text-align: left;
  }
  
  .review-module ul{
-    " . $color . "
+	" . $color . "
  }
  
  .review-module li{
-    " . $color . "
+	" . $color . "
  }
 
-</style>
+.submit-row {
+    display: -webkit-box;
+    display: -moz-box;
+    display: -ms-flexbox;
+    display: -webkit-flex;
+    display: flex;
+	align-items: center;
+	justify-content; center;
+	width: 100%;
+	height: 20%;
+}
+
+.submit-row input{
+	width: 100px; 
+	height: 30px;
+	$alignSubmit
+}
+
+.starRating:not(old){
+	" . $starDisplay . "
+	width : 10em;
+	height : 2em;
+	margin-left: 100px;
+}
+
+.starRating:not(old) > input{
+	opacity : 0;
+}
+
+.starRating:not(old) > label{
+	content : '';
+	display : block;
+	width : 3em;
+	height : 3em;
+	background : url('wp-content/plugins/cicr-testimonials/star-off.svg');
+	background-size : contain;
+}
+
+.starRating:not(old) > label:before{
+	content : '';
+	display : block;
+	flex-flow : row-reverse;
+	width : 3em;
+	height : 3em;
+	background : url('wp-content/plugins/cicr-testimonials/star-on.svg');
+	background-size : contain;
+	opacity : 0;
+	transition : opacity 0.2s linear;
+}
+
+.starRating {
+	display: block;
+	flex-flow: row-reverse;
+	margin-bottom: 12px;
+}
+
+.starRating input { margin: 0 5px;}
+.starRating:not(old) > label:hover:before,
+.starRating:not(old) > label:hover ~ label:before,
+.starRating:not(:hover) > :checked ~ label:before{
+  opacity : 1;
+}
+
+</style>      
+      
 <div style='clear:both;'></div>
 <div class='review-module'>
-	<h2 style='margin-top: 10px; align-self: flex-start'>WHAT OUR COFFEE LOVERS THINK OF THE BEST COFFEE FOR HOME</h2>
-	<div id='review-bubble-area'></div>
+	<div class='slider-div'>
+		<h2 style='margin-top: 10px; align-self: flex-start'>WHAT OUR COFFEE LOVERS THINK OF THE BEST COFFEE FOR HOME</h2>
+		$backend
+		<div id='review-bubble-area'></div>
+	</div>
+	<div class='review-area'>
+		<div style='width: 100%;'><h2>Hey! We're excited to hear what you have to say.</h2></div>
+		<form action='' class='testimonialForm' method='POST'>
+			<div style='width: 100%;'><input name='testimonial' type='textarea' placeholder='What you have to say'></input></div>
+			<div class='submit-row'>
+			    <span class='starRating'>
+			        <input id='rating5' type='radio' name='rating' value='5' style='order: 5;'> <label for='rating5'></label>
+			        <input id='rating4' type='radio' name='rating' value='4' style='order: 4;'> <label for='rating4'></label>
+			        <input id='rating3' type='radio' name='rating' value='3' style='order: 3;'> <label for='rating3'></label>
+			        <input id='rating2' type='radio' name='rating' value='2' style='order: 2;'> <label for='rating2'></label>
+			        <input id='rating1' type='radio' name='rating' value='1' style='order: 1;'> <label for='rating1'></label>
+			    </span>
+			    <input type='submit' id='reviewSubmit' value='Submit Review'>
+	    	</div>
+	    </form>
+	</div>
 	</div>
 	<div class='progressBar'><div></div></div>";
+	
     return $return_content;
 }
 
